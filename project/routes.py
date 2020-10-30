43268e337cfeb6c9c0aa718f4978a373bf2eb6ba
PanduAkshay <46069495+PanduAkshay@users.noreply.github.com>
2020-10-30 21:38:36 +0530

    Delete routes.py

--- project/routes.py
+++
@@ -1,8 +0,0 @@
-from project import app
-from flask import  render_template, url_for, Blueprint, request, session, redirect, escape
-from datetime import datetime
-
-@app.route('/')
-def home():
-	return render_template('layout.html')
-
